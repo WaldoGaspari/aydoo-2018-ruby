29aae9429e71ad2ed13fbf9207e319a05abeb547
WaldoGaspari <waldogaspari@gmail.com>
2018-05-27 20:20:34 -0300

    Se puede mostrar por navegador la serie de fibonacci de cualquier numero ya sea por lista o sumatoria.

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -2,7 +2,13 @@ require 'sinatra'
 require 'sinatra/json'
 require_relative '../fibo5/model/serie_fibonacci'
 
-get '/fibonacci/:numero' do
+get '/fibonacci/:numero/lista' do
   serieFibonacci = SerieFibonacci.new
-  json({ fibonacci: { limite: params[:numero].to_i}, lista: serieFibonacci.calcular_serie(params[:numero].to_i) })
+  json({ fibonacci: { limite: params[:numero].to_i}, lista: serieFibonacci.calcular_serie(params[:numero].to_i)})
+end
+
+get '/fibonacci/:numero/sumatoria' do
+  serieFibonacci = SerieFibonacci.new
+  resultado = serieFibonacci.calcular_serie(params[:numero].to_i)
+  json({ fibonacci: { limite: params[:numero].to_i}, sumatoria: serieFibonacci.calcular_sumatoria(resultado)})
 end
