72e4bed1aeddc5eca277c01683889b0c3c7b0042
WaldoGaspari <waldogaspari@gmail.com>
2018-05-28 23:19:18 -0300

    Se puede obtener por navegador la serie de fibonacci de cualquier numero ta sea por lista o sumatoria teniendo en cuenta todos los parametros opcionales.

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -3,6 +3,26 @@ require 'sinatra/json'
 require_relative '../fibo5/model/serie_fibonacci'
 require_relative '../fibo5/model/formato_serie_de_fibonacci'
 
+get '/fibonacci/:numero' do
+  serieFibonacci = SerieFibonacci.new
+  formato = FormatoSerieDeFibonacci.new
+  resultado = serieFibonacci.calcular_serie(params[:numero].to_i)
+
+  if (params[:solo] == "pares")
+    resultado = formato.calcular_numeros_pares(resultado)
+  end
+
+  if (params[:solo] == "impares")
+    resultado = formato.calcular_numeros_impares(resultado)
+  end
+
+  if (params[:sentido] == "inverso")
+    resultado = formato.invertir_resultado(resultado)
+  end
+
+  json({ fibonacci: { limite: params[:numero].to_i, lista: resultado} })
+end
+
 get '/fibonacci/:numero/lista' do
   serieFibonacci = SerieFibonacci.new
   formato = FormatoSerieDeFibonacci.new
