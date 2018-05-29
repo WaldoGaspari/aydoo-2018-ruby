2737c39480b58ec95d2c7a9467557ab52327edd3
WaldoGaspari <waldogaspari@gmail.com>
2018-05-28 22:11:08 -0300

    Se muestra por navegador la serie de fibonacci de cualquier numero teniendo en cuenta los parametros adicionales de sentido y solo.

--- fibo5/app.rb
+++ fibo5/app.rb
@@ -1,14 +1,29 @@
 require 'sinatra'
 require 'sinatra/json'
 require_relative '../fibo5/model/serie_fibonacci'
+require_relative '../fibo5/model/formato_serie_de_fibonacci'
 
 get '/fibonacci/:numero/lista' do
   serieFibonacci = SerieFibonacci.new
-  json({ fibonacci: { limite: params[:numero].to_i}, lista: serieFibonacci.calcular_serie(params[:numero].to_i)})
+  formato = FormatoSerieDeFibonacci.new
+  resultado = serieFibonacci.calcular_serie(params[:numero].to_i)
+  if (params[:solo] == "pares")
+    resultado = formato.calcular_numeros_pares(resultado)
+  end
+
+  if (params[:solo] == "impares")
+    resultado = formato.calcular_numeros_impares(resultado)
+  end
+
+  if (params[:sentido] == "inverso")
+    resultado = formato.invertir_resultado(resultado)
+  end
+
+  json({ fibonacci: { limite: params[:numero].to_i, lista: resultado} })
 end
 
 get '/fibonacci/:numero/sumatoria' do
   serieFibonacci = SerieFibonacci.new
   resultado = serieFibonacci.calcular_serie(params[:numero].to_i)
-  json({ fibonacci: { limite: params[:numero].to_i}, sumatoria: serieFibonacci.calcular_sumatoria(resultado)})
+  json({ fibonacci: { limite: params[:numero].to_i, sumatoria: serieFibonacci.calcular_sumatoria(resultado)} })
 end
