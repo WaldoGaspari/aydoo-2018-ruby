5da7d8f17a5d91983b965316ba61a86ca09eb680
WaldoGaspari <waldogaspari@gmail.com>
2018-07-14 12:52:38 -0300

    Se agregan dos ejemplos de plazo fijo tradicional para considerar cuando el resultado da con decimales.

--- finanzas/model/plazo_fijo.rb
+++ finanzas/model/plazo_fijo.rb
@@ -1,7 +1,7 @@
 class PlazoFijo
 
 	def calcular_ganancia_del_plazo_fijo (plazo, interes, monto)
-		resultado = (plazo/365) * (interes/100.00) * monto
+		resultado = ((plazo/365.00) * (interes/100.00) * monto).round(2)
 
 		return resultado
 	end
--- finanzas/spec/plazo_fijo_spec.rb
+++ finanzas/spec/plazo_fijo_spec.rb
@@ -8,5 +8,13 @@ describe 'PlazoFijo' do
   it 'calcular plazo fijo por un monto de 100000, plazo 365 dias e interes 10 porciento deberia dar 10000 de ganancia' do
     expect(plazoFijo.calcular_ganancia_del_plazo_fijo(365, 10, 100000)).to eq 10000.00
   end
+
+  it 'calcular plazo fijo por un monto de 100000, plazo 90 dias e interes 10 porciento deberia dar 2465 con 75 centavos de ganancia' do
+    expect(plazoFijo.calcular_ganancia_del_plazo_fijo(90, 10, 100000)).to eq 2465.75
+  end
+
+  it 'calcular plazo fijo por un monto de 150000, plazo 30 dias e interes 10 porciento deberia dar 1232 con 88 centavos de ganancia' do
+    expect(plazoFijo.calcular_ganancia_del_plazo_fijo(30, 10, 150000)).to eq 1232.88
+  end
 end
 
