790905c02d6e88c111a9a2c60c93889761c1fd88
WaldoGaspari <waldogaspari@gmail.com>
2018-07-15 11:31:35 -0300

    Primer ejemplo con entrada/salida para ver el funcionamiento del mismo.

--- finanzas/finanzas.rb
+++ finanzas/finanzas.rb
@@ -1,2 +1,16 @@
-class Finanzas 
+require_relative '../finanzas/model/compra_dolares'
 
+class Finanzas
+
+	compraDolares = CompraDolares.new
+	entrada = gets.chomp
+	entrada_separada = entrada.split
+	parametros = entrada_separada[2].split(',')
+	uno = parametros[1].to_i
+	dos = parametros[2].to_i
+	tres = parametros[3].to_i
+
+	resultado = (compraDolares.calcular_ganancia(uno, dos, tres))
+	puts " ganancia: #{resultado} "
+
+end
