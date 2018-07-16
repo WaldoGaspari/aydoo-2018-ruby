14dec3b39d6fd4edff88e8cd04913cac6c8e0b98
WaldoGaspari <waldogaspari@gmail.com>
2018-07-15 23:26:30 -0300

    Se logra la salida de la ganancia de las inversiones a traves de la entrada de parametros.

--- finanzas/finanzas.rb
+++ finanzas/finanzas.rb
@@ -1,16 +1,34 @@
 require_relative '../finanzas/model/compra_dolares'
+require_relative '../finanzas/model/plazo_fijo'
 
 class Finanzas
 
-	compraDolares = CompraDolares.new
 	entrada = gets.chomp
 	entrada_separada = entrada.split
-	parametros = entrada_separada[2].split(',')
-	uno = parametros[1].to_i
-	dos = parametros[2].to_i
-	tres = parametros[3].to_i
+	inversor = entrada_separada[1]
+	posicion = 2
+	total_ganancias = 0
+	compraDolares = CompraDolares.new
+	plazo_fijo = PlazoFijo.new
+	while posicion < entrada_separada.length do
+		parametros = entrada_separada[posicion].split(',')
 
-	resultado = (compraDolares.calcular_ganancia(uno, dos, tres))
-	puts " ganancia: #{resultado} "
+		if (parametros[0] == "dol")	
+			resultado = compraDolares.calcular_ganancia(parametros[1].to_i, parametros[2].to_i, parametros[3].to_i)
+		end
+
+		if (parametros[0] == "pft")
+			resultado = plazo_fijo.calcular_ganancia_del_plazo_fijo(parametros[1].to_i, parametros[2].to_i, parametros[3].to_i)
+		end
+
+		if (parametros[0] == "pfp")
+			resultado = plazo_fijo.calcular_ganancia_de_plazo_fijo_precancelable(parametros[1].to_i, parametros[2].to_i, parametros[3].to_i, parametros[4].to_i)
+		end
+
+		total_ganancias += resultado
+		posicion += 1
+	end
+	
+	puts " ganancia: #{total_ganancias} "
 
 end
